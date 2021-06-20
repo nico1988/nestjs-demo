01038f85390b7566bc17b8d17f11934bb79fea59
nico <410827024@qq.com>
2021-06-21 07:40:34 +0800

    feat: cat controllers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  Res,
+  Param,
+  Session,
+  Header,
+} from '@nestjs/common';
+import { Request } from 'express';
 
-@Controller('cats')
-export class CatsController {}
+@Controller({
+  path: 'cats',
+})
+export class CatsController {
+  @Header('Cache-Control', 'no-store')
+  @Header('nico', 'aaa')
+  @Get('/query/:id')
+  getQuery(@Param() params): string {
+    console.log('params:::', params);
+    let {id} = params;
+    return `your query is ${id}`;
+  }
+
+  @Get('/session')
+  getSession(@Session() session): string {
+    console.log('session:::', session);
+    return `your query is ${session}`;
+  }
+
+  @Get('/req')
+  getReq(@Req() request: Request): string {
+    console.log('request:::', request);
+    return `your query is req`;
+  }
+}
